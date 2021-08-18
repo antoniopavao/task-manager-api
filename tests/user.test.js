8f28ebd5c1e0d330a094b6f3d704072723af64c6
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2021-08-18 13:48:00 -0300

    Creating Advanced Assertions

--- tests/user.test.js
+++ tests/user.test.js
@@ -32,7 +32,7 @@ beforeEach(async () => {
 
 // SIGN UP USER
 test("Should signup a new user", async () => {
-  await request(app)
+  const response = await request(app)
     .post("/users")
     .send({
       name: "Antonio",
@@ -40,22 +40,51 @@ test("Should signup a new user", async () => {
       password: "apm174dh",
     })
     .expect(201);
+
+  // Assert that the database was changed correctly
+  const user = await User.findById(response.body.user._id);
+  expect(user).not.toBeNull();
+
+  //Assertions about the response
+  expect(response.body).toMatchObject({
+    user: {
+      name: "Antonio",
+      email: "a@example.com",
+    },
+    token: user.tokens[0].token,
+  });
+
+  //Asserting that the database is not storing plain text password
+  expect(user.password).not.toBe("apm174dh");
 });
 
-// Login user
+// Should Login existing user
 test("Should login existing user", async () => {
-  await request(app)
+  const response = await request(app)
     .post("/users/login")
     .send({
       email: userOne.email,
       password: userOne.password,
     })
     .expect(200);
+
+  // Assert that the database was changed correctly
+
+  //--> Fetching user from the database
+  const user = await User.findById(response.body.user._id);
+
+  // --> Expecting that the user is not null
+  expect(user).not.toBeNull();
+
+  //Assert that the token in response matches users second token
+  expect(response.body).toMatchObject({
+    token: user.tokens[1].token,
+  });
 });
 
 // Should not login nonexistent user
 test("Should not login nonexistent user", async () => {
-  await request(app)
+  const response = await request(app)
     .post("/users/login")
     .send({
       email: "somewrongemail@example.com",
@@ -66,7 +95,7 @@ test("Should not login nonexistent user", async () => {
 
 //Should get profile for a user
 test("Should get profile for a user", async () => {
-  await request(app)
+  const response = await request(app)
     .get("/users/me")
     .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
     .send()
@@ -75,19 +104,25 @@ test("Should get profile for a user", async () => {
 
 // Should not get profile for unauthenticated users
 test("Should not get profile for unauthenticated users", async () => {
-  await request(app).get("/users/me").send().expect(401);
+  const response = await request(app).get("/users/me").send().expect(401);
 });
 
 // Should delete account for user
 test("Should delete account for user", async () => {
-  await request(app)
+  const response = await request(app)
     .delete("/users/me")
     .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
     .send()
     .expect(200);
+
+  //--> Fetching user from the database
+  const user = await User.findById(userOneId);
+
+  // --> Expecting that the user is not null
+  expect(user).toBeNull();
 });
 
 // Should not delete account for unauthenticated user
 test("Should not delete account for unauthenticated user", async () => {
-  await request(app).delete("/users/me").send().expect(401);
+  const response = await request(app).delete("/users/me").send().expect(401);
 });
